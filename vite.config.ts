b59f68bd347158865de19fda1cb4ef7c95571c12
Sebastian Ptz <sebastian.ptz@outlook.com>
2026-02-13 23:38:36 +0100

    compatibility fix for loveable

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/no-gravity-berlin/',
+  base: mode === "production" ? "/no-gravity-berlin/" : "/",
   server: {
     host: "::",
     port: 8080,
