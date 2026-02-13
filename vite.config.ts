b4cee9f5bfe63e337fd4549de19c1bd9fa3b9849
Sebastian Ptz <sebastian.ptz@outlook.com>
2026-02-13 23:25:43 +0100

    set base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/no-gravity-berlin/',
   server: {
     host: "::",
     port: 8080,
